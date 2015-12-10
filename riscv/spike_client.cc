38f01465b6652349b4daef14d18b93860c202b2d
Pavel S. Smirnov <Paul.Smirnov.aka.sps@gmail.com>
2015-12-10 17:09:32 +0300

    FIX(end_of_clock()) reset this->m_was_transactions;

--- riscv/spike_client.cc
+++ riscv/spike_client.cc
@@ -52,7 +52,7 @@ vcs_device_agent::store(uint32_t addr, size_t len, uint8_t const* bytes)
     uint32_t data = 0u;
     for (size_t i = 0; i < len; ++i) {
         static_assert(CHAR_BIT == 8, "CHAR_BIT == 8 only");
-        data = (data <<  8) | uint32_t(*--p);
+        data = (data << 8) | uint32_t(*--p);
     }
     // LOGGER << "vcs_device_agent::store: len=" << len << std::endl;
     auto const ack = Client::instance().request(Request_type::write, addr, len, data);
@@ -68,15 +68,14 @@ vcs_device_agent::store(uint32_t addr, size_t len, uint8_t const* bytes)
 void
 vcs_device_agent::end_of_clock()
 {
-    if (this->m_was_transactions) {
-        return;
-    }
-    auto const ack = Client::instance().request(Request_type::skip);
-    if (ack->m_cmd == Request_type::skip) {
-    } else if (ack->m_cmd == Request_type::reset_state && ack->m_data != 0) {
-        throw Reset_active();
-    } else {
-        throw Exception();
+    if (!this->m_was_transactions) {
+        auto const ack = Client::instance().request(Request_type::skip);
+        if (ack->m_cmd == Request_type::skip) {
+        } else if (ack->m_cmd == Request_type::reset_state && ack->m_data != 0) {
+            throw Reset_active();
+        } else {
+            throw Exception();
+        }
     }
     this->m_was_transactions = false;
 }
