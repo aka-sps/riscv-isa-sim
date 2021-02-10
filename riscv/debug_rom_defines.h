87fbd9524d28f0a70d98dbdeb98740590e10f98c
Evgeny Alekhno <evgeny.alekhno@syntacore.com>
2021-02-10 00:17:51 +0000

    fix(debug_module): restored debug support

--- riscv/debug_rom_defines.h
+++ riscv/debug_rom_defines.h
@@ -4,20 +4,23 @@
 #define DEBUG_ROM_DEFINES_H
 
 // These are implementation-specific addresses in the Debug Module
-#define DEBUG_ROM_HALTED    0x100
-#define DEBUG_ROM_GOING     0x104
-#define DEBUG_ROM_RESUMING  0x108
-#define DEBUG_ROM_EXCEPTION 0x10C
+//#define DEBUG_ROM_HALTED    0x1100
+#define DEBUG_ROM_HALTED    0x1100
+#define DEBUG_ROM_GOING     0x1104
+#define DEBUG_ROM_RESUMING  0x1108
+#define DEBUG_ROM_EXCEPTION 0x110C
 
 // Region of memory where each hart has 1
 // byte to read.
-#define DEBUG_ROM_FLAGS 0x400
+#define DEBUG_ROM_FLAGS 0x1400
 #define DEBUG_ROM_FLAG_GO     0
 #define DEBUG_ROM_FLAG_RESUME 1
 
 // These needs to match the link.ld         
-#define DEBUG_ROM_WHERETO 0x300
-#define DEBUG_ROM_ENTRY   0x800
-#define DEBUG_ROM_TVEC    0x808
+#define DEBUG_ROM_WHERETO 0x1300
+#define DEBUG_ROM_ENTRY   0x1800
+#define DEBUG_ROM_TVEC    0x1808
+// Clobbered registers go here
+#define DEBUG_ROM_SCRATCH 0x2000
 
 #endif
