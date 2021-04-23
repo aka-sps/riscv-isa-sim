f3b5713c7f5e572bd082e96433be0d50d184ae9c
alexb-sc <alexey.belokon@syntacore.com>
2021-04-23 11:16:52 +0500

    fix(mpu): scr5 EAS 6.1.7 implemented, removed some magic numbers

--- riscv/mpu.h
+++ riscv/mpu.h
@@ -10,3 +10,12 @@
 #define MPU_SMW 0x100
 #define MPU_SMX 0x200
 #define MPU_LOCK 0x80000000
+#define MPU_MTYPE (3 << 16)
+#define MPU_CONTROL_MASK (0x3FF | MPU_MTYPE | MPU_LOCK)
+#define MPU_ADDR_MASK_32 (~(0x3FF | (3 << 30)))
+#define MPU_ADDR_MASK_64 (~(0x3FFULL | (0x3FFFFFFULL << 38)))
+
+#define MTYPE_C_WO (0 << 16)
+#define MTYPE_NC_SO (1 << 16)
+#define MTYPE_NC_WO (2 << 16)
+#define MTYPE_MMIO_NC_SO (3 << 16)
